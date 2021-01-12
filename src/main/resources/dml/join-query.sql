7f1085d95c7483795b701d3d0b66c80f47d110c1
CarlosSanabriaM <uo250707@uniovi.es>
2021-01-12 14:31:41 +0100

    style: Formatted sql query.

--- src/main/resources/dml/join-query.sql
+++ src/main/resources/dml/join-query.sql
@@ -1,7 +1,11 @@
-SELECT Directors.name AS director,
-       Movies.name AS movie,
-       Movies.nominatedToOscar AS nominatedToOscar
-FROM Movies
-INNER JOIN Directors
-ON Movies.directorId = Directors.id
-WHERE Movies.nominatedToOscar = TRUE
+SELECT
+    Directors.name AS director,
+    Movies.name AS movie,
+    Movies.nominatedToOscar AS nominatedToOscar
+FROM
+    Movies
+    INNER JOIN
+        Directors
+        ON Movies.directorId = Directors.id
+WHERE
+    Movies.nominatedToOscar = TRUE
