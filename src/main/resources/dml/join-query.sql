8d00c0171dccd25d8e3f1e3308545fde5f2eef55
CarlosSanabriaM <uo250707@uniovi.es>
2021-05-14 13:43:38 +0200

    feat: Modified SQL query to add the usage of a built-in function.

--- src/main/resources/dml/join-query.sql
+++ src/main/resources/dml/join-query.sql
@@ -1,5 +1,12 @@
 SELECT
-    GetDirectorsMoviesIdFunction(Directors.id, Movies.id) as id,
+    -- The id has the following format: "directorId:movieId"
+    -- There are 2 ways of obtaining the id in that format:
+    --   1. With a UDF (user defined function):
+    GetDirectorsMoviesIdFunction(Directors.id, Movies.id) as id,  -- used here only to show how they work
+    --   2. With a system (built-in) function:
+    -- CONCAT(Directors.id, ':', Movies.id) as id,  -- better solution. no need to code the function.
+
+    -- Other fields
     Directors.name AS director,
     Movies.name AS movie,
     Movies.nominatedToOscar AS nominatedToOscar
